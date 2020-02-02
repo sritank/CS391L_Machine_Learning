e590b22f138a260500705d7d6011a64bb30c8a97
Srinath <sritank@github.com>
2020-02-02 14:48:10 +0000

    added labels

--- Assign 1 Digits/PCA.py
+++ Assign 1 Digits/PCA.py
@@ -9,4 +9,13 @@ train_img = idx2numpy.convert_from_file('train-images-idx3-ubyte')
 #train_imagesfile = open(filename['images'],'rb')
 trainImgArr = Image.fromarray(train_img[2],'L');
 trainImgArr.show();
+
+train_img_2d = train_img.reshape(60000,28*28)
+
+train_lab = idx2numpy.convert_from_file('train-labels-idx1-ubyte')
+
+#covariance_matrix = np.matmul(train_lab.transpose(),train_lab)
+
+
+
 #keyboard.wait('Ctrl')
