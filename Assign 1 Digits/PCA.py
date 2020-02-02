b34f944b8ae3cbadcd8c1b43dce16a50b1c0a575
Srinath <sritank@github.com>
2020-02-02 18:44:29 +0000

    completed and working code for assignment 1

--- Assign 1 Digits/PCA.py
+++ Assign 1 Digits/PCA.py
@@ -3,19 +3,57 @@ import numpy as np
 import keyboard
 import idx2numpy
 from PIL import Image
-train_img = idx2numpy.convert_from_file('train-images-idx3-ubyte')
+from numpy import linalg as LA
+
+
+train_img = idx2numpy.convert_from_file('train-images-idx3-ubyte');
+test_img = idx2numpy.convert_from_file('t10k-images-idx3-ubyte');
 
 #filename = {'images' : 'train-images-idx3-ubyte' ,'labels' : 'train-labels-idx1-ubyte'}
 #train_imagesfile = open(filename['images'],'rb')
-trainImgArr = Image.fromarray(train_img[2],'L');
-trainImgArr.show();
+#trainImgArr = Image.fromarray(train_img[2],'L');
+#trainImgArr.show();
 
-train_img_2d = train_img.reshape(60000,28*28)
+train_img_2d = train_img.reshape(60000,28*28);
+test_img_2d = test_img.reshape(10000,28*28);
+col_sum = np.mean(train_img_2d, axis = 0);
 
-train_lab = idx2numpy.convert_from_file('train-labels-idx1-ubyte')
+train_img_2d = train_img_2d - col_sum;
 
-#covariance_matrix = np.matmul(train_lab.transpose(),train_lab)
+train_lab = idx2numpy.convert_from_file('train-labels-idx1-ubyte');
+test_lab = idx2numpy.convert_from_file('t10k-labels-idx1-ubyte');
+
+covariance_matrix = np.matmul(train_img_2d.transpose(),train_img_2d)/60000;
+
+w,v = LA.eig(covariance_matrix);
+
+t = 10
+V = v[:,0:t];
+W = w[0:t];
+
+train_img_V = np.matmul(train_img_2d, V);
+
+test_no = 159
+test_img_1 = test_img_2d[test_no];
+test_lab_1 = test_lab[test_no];
 
 
+
+testImgArr = Image.fromarray(test_img_1.reshape(28,28),'L');
+testImgArr.show();
+
+test_img_1 = test_img_1 - col_sum;
+
+test_img_1_V = np.matmul(test_img_1,V);
+
+metro_dist_test = np.abs(test_img_1_V-train_img_V);
+#train_img_reduced = np.matmul(train_img_2d,W)
+T = np.sum(metro_dist_test, axis=1);
+
+k = np.argmin(T)
+
+out = train_lab[k];
+print("k =   ",k, "   out=  ", out)
+print("ground truth value = ",test_lab_1)
 
 #keyboard.wait('Ctrl')
