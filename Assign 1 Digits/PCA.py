3171a64779d7922046faf6edc501b80e618f4936
Srinath <sritank@github.com>
2020-02-06 15:40:22 -0600

    completed hw

--- Assign 1 Digits/PCA.py
+++ Assign 1 Digits/PCA.py
@@ -27,38 +27,58 @@ covariance_matrix = np.matmul(train_img_2d.transpose(),train_img_2d)/60000;
 
 w,v = LA.eig(covariance_matrix);
 
-t = 100
+t = 20
 
 V = v[:,0:t];
 W = w[0:t];
 
 train_img_V = np.matmul(train_img_2d, V);
 
+correct_counter = 0;
+total_counter = 0;
+incorrect_test_index = np.array([]);
+incorrect_train_index = np.array([]);
 
-test_no = 1008
-test_img_1 = test_img_2d[test_no];
-test_lab_1 = test_lab[test_no];
+#test_no = 1008;
+for test_no in range(0,10000):
+    test_img_1 = test_img_2d[test_no];
+    test_lab_1 = test_lab[test_no];
+    total_counter = total_counter + 1
 
+    #testImgArr = Image.fromarray(test_img_1.reshape(28,28));
+    #testImgArr.show();
 
+    test_img_1 = test_img_1 - col_sum;
 
-testImgArr = Image.fromarray(test_img_1.reshape(28,28),'L');
-testImgArr.show();
+    test_img_1_V = np.matmul(test_img_1,V);
 
-test_img_1 = test_img_1 - col_sum;
+    metro_dist_test = np.abs(test_img_1_V-train_img_V);
+    #train_img_reduced = np.matmul(train_img_2d,W)
+    T = np.sum(metro_dist_test, axis=1);
 
-test_img_1_V = np.matmul(test_img_1,V);
+    i = np.argmin(T)
 
-metro_dist_test = np.abs(test_img_1_V-train_img_V);
-#train_img_reduced = np.matmul(train_img_2d,W)
-T = np.sum(metro_dist_test, axis=1);
+    out = train_lab[i];
+    if (out == test_lab_1):
+        correct_counter = correct_counter + 1
+    else:
+        # s = test_no
+        # print("came here s=",s)
+        incorrect_test_index = np.append(incorrect_test_index,[test_no]);
+        incorrect_train_index = np.append(incorrect_train_index,[i]);
 
-k = np.argmin(T)
+# print("k =   ",k, "   out=  ", out)
+# print("ground truth value = ",test_lab_1)
+print("total images", total_counter);
+print("correctly identified images", correct_counter);
 
-out = train_lab[k];
-print("k =   ",k, "   out=  ", out)
-print("ground truth value = ",test_lab_1)
+# for j in range(0,10):#enumerate(incorrect_train_index, start=0):
+# j=5
+# out_images = train_img[incorrect_train_index[j].astype(int)];
+# incorrect_test_images = test_img[incorrect_test_index[j].astype(int)];
+# inc_trainImg = Image.fromarray(out_images);
+# inc_trainImg.show()
+# inc_testImg = Image.fromarray(incorrect_test_images);
+# inc_testImg.show();
 
-out_image = train_img[k];
-matchImg = Image.fromarray(out_image,'L');
-matchImg.show()
 #keyboard.wait('Ctrl')
