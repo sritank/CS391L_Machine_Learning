37d6b53d7b7f957ea2ff1be241955c376030a7a8
Srinath <sritank@github.com>
2020-02-25 19:50:35 -0600

    finished assign

--- Assign 2 ICA/ICA.py
+++ Assign 2 ICA/ICA.py
@@ -11,53 +11,51 @@ import ipdb;
 import ica_functions
 from scipy.io import loadmat;
 from IPython.display import Audio
+import matplotlib.pyplot as plt
 
 sounds = loadmat('./sounds.mat')['sounds'];
 
-test = loadmat('./icaTest.mat');
-test = loadmat('./sounds.mat');
-
+# test = loadmat('./icaTest.mat');
 # A = test['A'];
 # U = test['U'];
 
+test = loadmat('./sounds.mat');
 U = test['sounds'];
-#A = np.array([1, 1, 1, 1, 1],[0.5, 0.5, 0, 1, 1],[0, 0, 0.5, 0.5, 1],[0.5, 0, 0.5, 1, 1],[0, 0.7, 0.7, 0.1, 0.3])
+#initializing mixing matrix
 A = ([0.5, 1, 0.2, 1, 0.3],[0.5, 0.5, 0.2, 0.6, 0.1],[0.8, 0.4, 0.5, 0.5, 0.31],[0.5, 0.4, 0.5, 1, 1],[0.2, 0.7, 0.7, 0.1, 0.3])
-# A = np.array([[0]*5]*5)
-
-print(A)
-#print(A[1,:])
-#A = np.array([1, 1, 1, 1, 1]);
+print("A=",A)
 A=np.array(A)
 m = A.shape[0];
-
-#A[1,:]=np.array([0.5, 0.5, 0, 1, 1]) #,[0, 0, 0.5, 0.5, 1],[0.5, 0, 0.5, 1, 1],[0, 0.7, 0.7, 0.1, 0.3])
 n = A.shape[1];
-W = np.random.rand(m,n);
-# W = [[0.79722847, 0.74092872, 0.96148456, 0.33723867, 0.42588017],
-#        [0.2934053 , 0.82959767, 0.88318573, 0.41958464, 0.63933106],
-#        [0.57606894, 0.76955245, 0.07654651, 0.64246145, 0.90654973],
-#        [0.2412909 , 0.10643112, 0.4064552 , 0.32060839, 0.76532299],
-#        [0.44236227, 0.02811083, 0.70773259, 0.39954014, 0.8544893 ]];
-# W = np.array(W)
+
+W = ([[0.41113469, 0.21755247, 0.55648479, 0.15306791, 0.84595753],
+       [0.34862363, 0.34633332, 0.13030717, 0.80047687, 0.8880103 ],
+       [0.23412216, 0.93441739, 0.53875375, 0.79915107, 0.32011519],
+       [0.24549826, 0.11758528, 0.25647   , 0.40310727, 0.05423368],
+       [0.84298051, 0.12147498, 0.99985129, 0.17120442, 0.6393325 ]]);
+
+W = np.array(W)
+#assuming fixed W1, uncomment if W1 is to be random
+# W = np.random.rand(m,n);
+
+correl = np.array([0,0,0,0,0]) #initializing correlation matrix
+
+cor_iter = [[0],[0],[0],[0],[0]]
 
 X = np.matmul(A,U)
 t = X.shape[1];
 
-XRowSum = X.sum(axis=1)
-print(XRowSum)
 
-#X=X-np.matlib.repmat(XRowSum,t,1).transpose();
-
-eta = 0.002;
+eta = 0.01;
 dW = np.zeros((n,n))+.01; #dW becomes a shallow copy if dW=W
 count = 0;
-W1=W; X1=X;
+# W1=W; X1=X;
 # ipdb.set_trace();
-# W0,WX,couter = ica_functions.performICA(W1, X1, eta, 1e4, 1e-4);
 tol = 1;
 tol_limit = 1e-6;
 max_iter = 1e5;
+# ipdb.set_trace();
+#Iterating to calculate unmixing matrix
 while (count<max_iter and tol > tol_limit):
     Y = W.dot(X)
 
@@ -66,16 +64,26 @@ while (count<max_iter and tol > tol_limit):
     dW = eta*((np.eye(n) + (1-2*Z).dot(np.transpose(Y))/t).dot(W))
     W = W + dW
     count = count+1;
+
     tol = LA.norm(dW)/LA.norm(W)
-#Audio(X[0,:], rate=11025)
+    # change this order for W.dot(X)[] accordingly if starting value of W is changed
+    #retrieving correlation with corresponding retrieved signals
+    cor_iter[0] = np.corrcoef(U[0,:],W.dot(X)[2,:])[0,1]
+    cor_iter[1] = np.corrcoef(U[1,:],W.dot(X)[4,:])[0,1]
+    cor_iter[2] = np.corrcoef(U[2,:],W.dot(X)[1,:])[0,1]
+    cor_iter[3] = np.corrcoef(U[3,:],W.dot(X)[0,:])[0,1]
+    cor_iter[4] = np.corrcoef(U[4,:],W.dot(X)[3,:])[0,1]
+    correl = np.vstack([correl,cor_iter]) # updating correlation matrix with iteration
 
-
+#retrieving estimate of source signals
+Uhat = W.dot(X);
+#Playing back audio at 44100/4 Hz
 freq = 11025;
 sd.play(U[0,:],freq,blocking = True)
+sd.play(Uhat[0,:]/27.5,freq, blocking=True)
 
-Uhat = W.dot(X);
-# Uhat1 = W0.dot(X)
-sd.play(Uhat[0,:],freq, blocking=True)
+#Saving data for plotting
+data = {"U": U, "What":W, "X":X, "Uhat":Uhat, "correl":correl}
+pkl.dump(data,open("plot_data_ICA.p","wb"));
 # sd.play(Uhat1[0,:],freq, blocking=True)
 ipdb.set_trace();
-#Audio(X, rate=11025)
---
+++ Assign 2 ICA/plotting.py
@@ -0,0 +1,98 @@
+import struct as st
+import numpy as np
+import keyboard
+import idx2numpy
+from PIL import Image
+from numpy import linalg as LA
+from numpy import matlib
+import sounddevice as sd
+import pickle as pkl
+# import ipdb;
+import ica_functions
+from scipy.io import loadmat;
+from IPython.display import Audio
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 14})
+# plt.rcParams.update({'font.weight': 'bold'})
+
+data = pkl.load(open("plot_data_ICA.p","rb"));
+Uhat = data['Uhat']; U = data['U']; X = data['X']; What = data['What']; correl = data['correl'];
+
+# A = ([0.5, 1, 0.2, 1, 0.3],[0.5, 0.5, 0.2, 0.6, 0.1],[0.8, 0.4, 0.5, 0.5, 0.31],[0.5, 0.4, 0.5, 1, 1],[0.2, 0.7, 0.7, 0.1, 0.3])
+
+# ax = plt.axes()
+#Plotting source signals
+l1, = plt.plot(U[0,:], color='skyblue', linewidth=2, label = 'original signal 1')
+l2, = plt.plot(U[1,:]+2.5, color='red', linewidth=2, label = 'original signal 2')
+l3, = plt.plot(U[2,:]+5, color='green', linewidth=2, label = 'original signal 3')
+l4, = plt.plot(U[3,:]+7.5, color='black', linewidth=2, label = 'original signal 4')
+l5, = plt.plot(U[4,:]+10, color='yellow', linewidth=2, label = 'original signal 5')
+plt.ylabel('signal level')
+plt.ylim([-1.5,12])
+plt.xlabel('time sample')
+# plt.legend(handles = [l1,l2,l3,l4,l5])
+plt.title('Original signals');# (y offset is for plotting purposes)')
+plt.savefig('Original_signals.png')
+plt.close();
+
+#Plotting mixed signals
+l1, = plt.plot(X[0,:], color='skyblue', linewidth=2, label = 'mixed signal 1')
+l2, = plt.plot(X[1,:]+2.5, color='red', linewidth=2, label = 'mixed signal 2')
+l3, = plt.plot(X[2,:]+5, color='green', linewidth=2, label = 'mixed signal 3')
+l4, = plt.plot(X[3,:]+7.5, color='black', linewidth=2, label = 'mixed signal 4')
+l5, = plt.plot(X[4,:]+10, color='yellow', linewidth=2, label = 'mixed signal 5')
+plt.rcParams.update({'font.size': 14})
+# plt.rcParams.update({'font.weight': 'bold'})
+plt.ylabel('signal level')
+plt.ylim([-1.5,12])
+plt.xlabel('time sample')
+# plt.legend(handles = [l1,l2,l3,l4,l5])
+plt.title('Mixed signals');# (y offset is for plotting purposes)')
+plt.savefig('Mixed_signals.png')
+plt.close();
+
+#Plotting retrieved signals
+l1, = plt.plot(Uhat[0,:], color='skyblue', linewidth=2, label = 'retrieved signal 1')
+l2, = plt.plot(Uhat[1,:]+35, color='red', linewidth=2, label = 'retrieved signal 2')
+l3, = plt.plot(Uhat[2,:]+70, color='green', linewidth=2, label = 'retrieved signal 3')
+l4, = plt.plot(Uhat[3,:]+105, color='black', linewidth=2, label = 'retrieved signal 4')
+l5, = plt.plot(Uhat[4,:]+140, color='yellow', linewidth=2, label = 'retrieved signal 5')
+plt.rcParams.update({'font.size': 14})
+# plt.rcParams.update({'font.weight': 'bold'})
+plt.ylabel('signal level')
+plt.ylim([-20,155])
+plt.xlabel('time sample')
+# plt.legend(handles = [l1,l2,l3,l4,l5])
+plt.title('Retrieved signals');# (y offset is for plotting purposes)')
+plt.savefig('Retrieved_signals.png')
+plt.close();
+
+#Plotting Correlation between signals
+plt.rcParams.update({'font.size': 10})
+l1, = plt.plot(correl[:,0], color='skyblue', linewidth=2, label = 'correlation source 1')
+l2, = plt.plot(correl[:,1], color='red', linewidth=2, label = 'correlation source 2')
+l3, = plt.plot(correl[:,2], color='green', linewidth=2, label = 'correlation source 3')
+l4, = plt.plot(correl[:,3], color='black', linewidth=2, label = 'correlation source 4')
+l5, = plt.plot(correl[:,4], color='yellow', linewidth=2, label = 'correlation source 5')
+# plt.rcParams.update({'font.weight': 'bold'})
+plt.ylabel('Cross Correlation coefficient')
+plt.ylim([-2,4])
+plt.xlabel('iterations')
+plt.legend(handles = [l1,l2,l3,l4,l5])
+plt.title('Correlation between source and corresponding retrieved signals');# (y offset is for plotting purposes)')
+plt.savefig('Correlation_signals.png')
+plt.close();
+
+#Plotting Homer simpson audio
+plt.rcParams.update({'font.size': 14})
+l1, = plt.plot(U[0,:], color='green', linewidth=4, label = 'original signal')
+l2, = plt.plot(Uhat[2,:]/9.49, color='red', linewidth=1, label = 'retrieved scaled signal')
+# plt.rcParams.update({'font.weight': 'bold'})
+plt.ylabel('signal level')
+plt.ylim([-1,2])
+plt.xlim([21750,22250])
+plt.legend(handles = [l1,l2])
+plt.xlabel('time sample')
+plt.title('Original vs retrieved scaled signal')
+plt.savefig('Homer_Simpson_audio.png')
+plt.close();
