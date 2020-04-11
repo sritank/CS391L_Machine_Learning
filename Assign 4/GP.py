bf8de302ba078cb614dec495d5153889d0825d49
Srinath <sritank@github.com>
2020-04-11 11:50:10 -0500

    working prediction error

--- Assign 4/GP.py
+++ Assign 4/GP.py
@@ -24,7 +24,7 @@ x_t = 'frame'
 #***************Reading data from file********************************************
 
 
-window_size=30
+
 sigma_l_arr = 0;
 sigma_f_arr = 0;
 sigma_n_arr = 0;
@@ -91,6 +91,8 @@ with open('./data_GP/AG/block5-UNWEIGHTED-SLOW-NONDOMINANT-RANDOM/20161213210121
          data5 = np.vstack([data5, np.array([row[x_t], row[marker_x], row[marker_c]],dtype=float)]);
 data5=np.delete(data5,0,0);
 data = np.zeros((1010,2));
+
+# ************** mixing 5 traces **********************************
 counter=0;
 flag=0;
 for i in range(0,1010):
@@ -123,9 +125,40 @@ for i in range(0,1010):
         else:
             r=random.randint(1,5);
 
+# ************** mixing 2 traces **********************************
+counter=0;
+flag=0;
+for i in range(0,1010):
+    r=random.randint(1,2);
+    flag=0;
+    while flag==0:
+        if data1[i,2]<0 and data2[i,2]<0:
+            flag=1;
+            continue;
+        elif r==1 and data1[i,2]>0:
+            data[counter,:] = data1[i,:-1];
+            counter=counter+1;
+            flag=1;
+        elif r==2 and data2[i,2]>0:
+            data[counter,:] = data2[i,:-1];
+            counter=counter+1;
+            flag=1;
+        else:
+            r=random.randint(1,2);
 
+
+# counter=0;
+# for i in range(0,1010):
+#     if data1[i,2]<=0:
+#         continue;
+#     else:
+#         data[counter,:] = data1[i,:-1];
+#         counter=counter+1;
+
+# data = data1[:,:-1]
+window_size=1000
 #************************ starting the window process ***********************************************
-for frame_start in range(0,900):
+for frame_start in range(0,1):#data.shape[0]-100-window_size):
 
     # ipdb.set_trace()
     # L = np.max([data1.shape,data2.shape,data3.shape,data4.shape,data5.shape])
@@ -140,19 +173,22 @@ for frame_start in range(0,900):
 
 
     YY = data_curr[:,1].reshape(-1,1);
-
+    Xpredict = data[frame_start+window_size:frame_start+window_size+15,0];
+    Ypredict = data[frame_start+window_size:frame_start+window_size+15,1];
     sigma_f = 1;
-    sigma_l=1;
-    sigma_n = 0#-np.Inf;#1;#-2;
-    eta = 1e-3;
+    sigma_l=2;
+    sigma_n = -3#-np.Inf;#1;#-2;
+    eta = 1e-2;
 
     L = XX.size
     K=np.zeros([L,L]);
     k = np.zeros([L,L]);
     kl = np.zeros([L,L]);
-    tol=1;
+    tol=0.5;
     dPdf = np.array([[1]]); dPdl = np.array([[1]]); dPdn = np.array([[1]]);
-    err = (np.abs(dPdf[0,0])+np.abs(dPdl[0,0])+np.abs(dPdn[0,0]));
+    err = 2;#(np.abs(dPdf[0,0])+np.abs(dPdl[0,0])+np.abs(dPdn[0,0]));
+    err_prev = 10;
+    err_prev_2 = 100;
     count=0;
 
     Xii = np.multiply(XX,XX);
@@ -161,10 +197,11 @@ for frame_start in range(0,900):
     # ipdb.set_trace();
     XXi_XXj = Xii+Xjj-2*XX.dot(XX.transpose());
 
+    # ipdb.set_trace()
 
-
-    while err >tol:
-        if count>1e4:
+    # while err < err_prev:# or err_prev < err_prev_2:
+    while err > tol:
+        if count>3e3:
             break
 
         kp = np.exp(sigma_f)*np.exp(-0.5*np.exp(sigma_l)*XXi_XXj);
@@ -183,9 +220,30 @@ for frame_start in range(0,900):
         dPdn = 0.5*(YY.transpose()).dot(Qinv).dot(Qinv).dot(YY)*np.exp(sigma_n) - 0.5*np.trace(Qinv*np.exp(sigma_n));
 
         sigma_f = sigma_f + eta*dPdf;#-eta*dPdf doesn't converge
-        sigma_l = sigma_l + eta*dPdl;#-eta*dPdl doesn't converge
+        sigma_l = sigma_l + eta**2*dPdl;#-eta*dPdl doesn't converge
         sigma_n = sigma_n + eta*dPdn;#-eta*dPdn doesn't converge
+
+#************ Calculate prediction error **************************
+        # Xpredict = Xpredict.reshape(-1,1)
+        # Xpredict = Xpredict.transpose()
+        # Lpredict = Xpredict.size
+        # XXpred1 = np.matlib.repmat(XX,1,Lpredict)
+        # XXpred1 = np.multiply(XXpred1,XXpred1)
+        # XXpred2 = np.matlib.repmat(Xpredict,L,1)
+        # XXpred2 = np.multiply(XXpred2,XXpred2)
+        # XXpred = XXpred1+XXpred2-2*XX.dot(Xpredict)
+        #
+        # # Xpred_cov = np.multiply(np.matlib.repmat(Xpredict,Lpredict,1),np.matlib.repmat(Xpredict,Lpredict,1)) + (np.multiply(np.matlib.repmat(Xpredict,Lpredict,1),np.matlib.repmat(Xpredict,Lpredict,1))).transpose() -2*Xpredict.T.dot(Xpredict);
+        #
+        # KXpredX = np.exp(sigma_f)*np.exp(-0.5*np.exp(sigma_l)*XXpred.transpose())
+        # kp = np.exp(sigma_f)*np.exp(-0.5*np.exp(sigma_l)*XXi_XXj) + np.eye(L)*np.exp(sigma_n);
+        # mXpred = KXpredX.dot(LA.inv(kp)).dot(YY)
+
+        # err_prev_2 = err_prev;
+        # err_prev = err;
+        # err = np.sqrt(np.mean(np.multiply(Ypredict-mXpred, Ypredict-mXpred))); #(np.abs(dPdf[0,0])+np.abs(dPdl[0,0])+np.abs(dPdn[0,0]));
         err = (np.abs(dPdf[0,0])+np.abs(dPdl[0,0])+np.abs(dPdn[0,0]));
+
         count=count+1;
         # print("count=",count)
         # print("err=",err)
@@ -201,10 +259,10 @@ for frame_start in range(0,900):
     print("sigma_n=",sigma_n)
     print("sigma_f=",sigma_f)
     print("det=",LA.det(Q))
-
-    sigma_n_arr = np.vstack([sigma_n_arr, sigma_n]);
-    sigma_f_arr = np.vstack([sigma_f_arr, sigma_f]);
-    sigma_l_arr = np.vstack([sigma_l_arr, sigma_l]);
+    if count<5000:
+        sigma_n_arr = np.vstack([sigma_n_arr, sigma_n]);
+        sigma_f_arr = np.vstack([sigma_f_arr, sigma_f]);
+        sigma_l_arr = np.vstack([sigma_l_arr, sigma_l]);
 
 
 
@@ -219,7 +277,7 @@ sigma_f_arr = np.delete(sigma_f_arr,0)
 sigma_n_arr = np.delete(sigma_n_arr,0)
 
 
-Xstar = np.linspace(XX[0],XX[-1],1000);#XX+0.5;
+Xstar = np.linspace(XX[0],XX[-1],window_size*10);#XX+0.5;
 Xstar = Xstar.reshape(-1,1)
 Xstar = Xstar.transpose()
 L1 = Xstar.size
@@ -245,7 +303,7 @@ plt.errorbar(Xstar.T,mXstar, yerr=np.sqrt(np.diagonal(PXstar))*2)
 
 # plt.plot(Xstar.T,Ym[0],'ro');
 plt.plot(XX,YY,'g--')
-
+plt.plot(XX,YY,'ro')
 plt.savefig('./plots/marker_0_x.png');
 plt.show()
 ipdb.set_trace();
@@ -255,8 +313,11 @@ l1, = plt.plot(sigma_f_arr,'b', label='sigma_f')
 l2, = plt.plot(sigma_l_arr,'g', label='sigma_l')
 
 l3, = plt.plot(sigma_n_arr,'r', label='sigma_n')
-plt.legend(handles = [l1,l2, l3])
+l4, = plt.plot(data[:,0],data[:,1],'k', label='trajectory')
+plt.legend(handles = [l1,l2,l3,l4])
+
 plt.savefig('./plots/marker_'+marker+'_x_hyperparams.png');
+
 plt.show()
 
 hyper_param_arr = {"f": sigma_f_arr, "l":sigma_l_arr, "n":sigma_n_arr, "X":data[:,0], "Y":data[:,1]}
